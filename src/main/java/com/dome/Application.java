efe27a4ed22355bb447042637919d56dd01d8208
huangzhi <huangz@g2l-service.com>
2018-12-08 18:40:35 +0800

    移除mybits相关配置

--- src/main/java/com/dome/Application.java
+++ src/main/java/com/dome/Application.java
@@ -3,7 +3,6 @@ package com.dome;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@MapperScan("com.dome.*.mapper")
 @SpringBootApplication
 public class Application {
 
